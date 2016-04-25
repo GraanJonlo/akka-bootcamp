d365ee2c758de1a44f9823f646f7c4e5b75351a7
Andrew Grant <andy.a.grant@gmail.com>
2016-04-25 07:28:16 +0100

    Lessons 1.1-1.3 completed

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -1,48 +1,32 @@
-﻿using System;
-﻿using Akka.Actor;
+﻿using System.Threading.Tasks;
+using Akka.Actor;
 
 namespace WinTail
 {
-    #region Program
-    class Program
+    public static class Program
     {
-        public static ActorSystem MyActorSystem;
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            // initialize MyActorSystem
-            // YOU NEED TO FILL IN HERE
-
-            PrintInstructions();
-
-            // time to make your first actors!
-            //YOU NEED TO FILL IN HERE
-            // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
-            // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
-
-
-            // tell console reader to begin
-            //YOU NEED TO FILL IN HERE
-
-            // blocks the main thread from exiting until the actor system is shut down
-            MyActorSystem.AwaitTermination();
+            AsyncMain().Wait();
         }
 
-        private static void PrintInstructions()
+        private static async Task AsyncMain()
         {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+            using (var myActorSystem = ActorSystem.Create("MyActorSystem"))
+            {
+                Props consoleWriterProps = Props.Create<ConsoleWriterActor>();
+                IActorRef consoleWriterActor = myActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
+
+                Props validationActorProps = Props.Create<ValidationActor>(consoleWriterActor);
+                IActorRef validationActor = myActorSystem.ActorOf(validationActorProps, "validationActor");
+
+                Props consoleReaderProps = Props.Create<ConsoleReaderActor>(validationActor);
+                IActorRef consoleReadActor = myActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
+
+                consoleReadActor.Tell(ConsoleReaderActor.StartCommand);
+
+                await myActorSystem.WhenTerminated;
+            }
         }
     }
-    #endregion
 }
---
+++ src/Unit-1/DoThis/ValidationActor.cs
@@ -0,0 +1,42 @@
+using Akka.Actor;
+
+namespace WinTail
+{
+    class ValidationActor : UntypedActor
+    {
+        private readonly IActorRef _consoleWriterActor;
+
+        public ValidationActor(IActorRef consoleWriterActor)
+        {
+            _consoleWriterActor = consoleWriterActor;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            var msg = message as string;
+            if (string.IsNullOrEmpty(msg))
+            {
+                _consoleWriterActor.Tell(new Messages.NullInputError("No input received."));
+            }
+            else
+            {
+                if (IsValid(msg))
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you! Message was valid."));
+                }
+                else
+                {
+                    _consoleWriterActor.Tell(new Messages.ValidationError("Invalid: input had odd number of characters."));
+                }
+            }
+
+            Sender.Tell(new Messages.ContinueProcessing());
+        }
+
+        private static bool IsValid(string message)
+        {
+            var valid = message.Length % 2 == 0;
+            return valid;
+        }
+    }
+}
